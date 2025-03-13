9692f18ea497ac4ed9d8929b5ffdb821719a6f6b
Nareshkumarbalamurugan <153281056+Nareshkumarbalamurugan@users.noreply.github.com>
2025-03-13 09:07:59 +0530

    Update binary_counter.cpp

--- binary_counter.cpp
+++ binary_counter.cpp
@@ -4,21 +4,27 @@
 class BinaryCounter {
 private:
     std::vector<bool> counter;
+    int totalCost;  // Total number of bit flips (cost) performed.
 
 public:
     BinaryCounter(int bits) {
         counter.resize(bits, false);
+        totalCost = 0;  // Initialize the total cost to 0.
     }
 
     void increment() {
+        int cost = 0;
         int i = 0;
         while (i < counter.size() && counter[i]) {
             counter[i] = false;
+            cost++;
             i++;
         }
         if (i < counter.size()) {
             counter[i] = true;
+            cost++;
         }
+        totalCost += cost;
     }
 
     void display() {
@@ -26,6 +32,10 @@ public:
             std::cout << counter[i];
         }
         std::cout << std::endl;
+    }
+
+    void displayTotalCost() {
+        std::cout << "Total cost (bit flips): " << totalCost << std::endl;
     }
 };
 
@@ -37,5 +47,6 @@ int main() {
         counter.display();
     }
 
+    counter.displayTotalCost();
     return 0;
 }
